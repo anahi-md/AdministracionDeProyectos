ff7f579ce91eb8ad8e56235a648bdcfee444d703
anahi-md <anahimdz_04@hotmail.com>
2025-11-17 03:38:09 -0600

    EQ3 Seguimiento de Juegos

--- Proyecto/Backend/CampusVirtual/Modules/Equipo3/Logic/games_logic.py
+++
@@ -1,4 +0,0 @@
-from Modules.Equipo3.Data.games_data import games_db
-
-def get_all_games():
-    return games_db
--- Proyecto/Backend/CampusVirtual/Modules/Equipo3/Models/games_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Game(BaseModel):
-    id: int
-    equipo_local: str
-    equipo_visitante: str
-    marcador_local: int
-    marcador_visitante: int
-    estado: str
